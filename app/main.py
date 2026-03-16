3422812de52c25a0b8099793e1f3cbd4a864a535
Ritu Dhanani <ritu.taglineinfo.in>
2026-03-16 15:12:34 +0530

    create user and task  models

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.database import Base, engine
+
+from app.models import user, task
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def home():
+    return {"message": "Task Manager API Running"}
