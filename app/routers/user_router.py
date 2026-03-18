ceb79b29b323611238dbc65357878f1717b7dcff
Ritu Dhanani <ritu.taglineinfo.in>
2026-03-18 11:31:00 +0530

    implement refresh token functionality

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
+from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas.user_schema import UserCreate, UserLogin
 from app.services.user_service import create_user, authenticate_user
-from app.services.jwt_service import create_access_token
+from app.services.jwt_service import ALGORITHM, SECRET_KEY, create_access_token, create_refresh_token
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -21,9 +22,29 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
     if not db_user:
         raise HTTPException(status_code=400, detail="Invalid credentials")
 
-    token = create_access_token({"sub": db_user.email})
+    access_token = create_access_token({"sub": db_user.email})
+    refresh_token = create_refresh_token({"sub": db_user.email})
 
     return {
-        "access_token": token,
+        "access_token": access_token,
+        "refresh_token": refresh_token,
         "token_type": "bearer"
     }
+
+
+@router.post("/refresh")
+def refresh_token(refresh_token: str):
+    try:
+        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        if payload.get("type") != "refresh":
+            raise HTTPException(status_code=401, detail="Invalid token type")
+
+        email = payload.get("sub")
+
+        new_access_token = create_access_token({"sub": email})
+
+        return {"access_token": new_access_token}
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
--- app/services/jwt_service.py
+++ app/services/jwt_service.py
@@ -8,11 +8,18 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
-
+REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expire, "type": "access"})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire, "type": "refresh"})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
