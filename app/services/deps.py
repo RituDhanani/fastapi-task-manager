35363a8a7e0c5b5ade8249d734c7209e9f999472
Ritu Dhanani <ritu.taglineinfo.in>
2026-03-23 11:39:34 +0530

    add postman thing

--- app/services/deps.py
+++ app/services/deps.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Header
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from app.database import get_db
@@ -12,20 +12,25 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 
 
-def get_current_user(token: str, db: Session = Depends(get_db)):
+def get_current_user(
+    authorization: str = Header(None),  
+    db: Session = Depends(get_db)
+):
+    if not authorization:
+        raise HTTPException(status_code=401, detail="Authorization header missing")
+
     try:
+        token = authorization.split(" ")[1]  # Bearer <token>
+
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email = payload.get("sub")
-
-        if email is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
 
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
     user = db.query(User).filter(User.email == email).first()
 
-    if user is None:
+    if not user:
         raise HTTPException(status_code=401, detail="User not found")
 
     return user
